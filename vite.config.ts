6b93a3c0d6fb8025c07e21e03fbbafada7184d5d
pavanabega <144033799+pavanabega@users.noreply.github.com>
2025-08-21 20:24:21 +0530

    changed build path docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/spice-craft-kitchen/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+    sourcemap: mode === "development" ? "inline" : false,
+  },
   server: {
     host: "::",
     port: 8080,
