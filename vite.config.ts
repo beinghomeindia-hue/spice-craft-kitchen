83b637b08574422d340cb9680e38da275909ddde
pavanabega <144033799+pavanabega@users.noreply.github.com>
2025-08-22 12:18:58 +0530

    base fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/spice-craft-kitchen/",
+  base:"/spice-craft-kitchen/",
   build: {
     outDir: "docs"
   },
